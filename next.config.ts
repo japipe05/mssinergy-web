8bb5a9aad8c693565188bea6dddf05d6c6107f87
Andres Felipe Rodriguez roa <felipehuchija@gmail.com>
2025-04-07 22:15:11 -0500

    p

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: 'export', // ⚠️ Necesario para usar `next export`
 };
 
 export default nextConfig;
